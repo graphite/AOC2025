23638ce3b7aaa9d2d448401021583b8687b178fa
Anton Astafiev <anton.astafiev@nearone.org>
2025-12-04 09:41:46 +0100

    Day 4

---
+++ 4/small.py
@@ -0,0 +1,37 @@
+import sys
+
+
+def is_valid(x, y, m):
+    if x < 0 or y < 0 or x >= len(m) or y >= len(m[0]):
+        return False
+    return True
+
+
+def neighbors(x, y, m):
+    return [
+        (x, y) for (x, y) in (
+            (x-1, y), (x+1, y), (x, y-1), (x, y+1),
+            (x-1, y-1), (x-1, y+1), (x+1, y-1), (x+1, y+1),
+        ) if is_valid(x, y, m)
+    ]
+
+
+def main(stream):
+    layout = [l.strip() for l in stream]
+    n = 0
+    for x in range(len(layout)):
+        for y in range(len(layout[0])):
+            if layout[x][y] != '@':
+                continue
+            a = 0
+            for i, j in neighbors(x, y, layout):
+                if layout[i][j] == '@':
+                    a += 1
+            if a < 4:
+                n += 1
+    print(n)
+
+
+if __name__ == '__main__':
+    infile = sys.argv[1] if len(sys.argv) > 1 else 'sample.txt'
+    main(open(infile))
